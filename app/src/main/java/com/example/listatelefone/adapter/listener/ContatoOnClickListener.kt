538fe5870ad6e8bf34ba7c3de6a3642a5d07fab4
antoniocarlos98 <antoniocarlos0724@gmail.com>
2024-02-02 00:36:41 -0300

    app

--- app/src/main/java/com/example/listatelefone/adapter/listener/ContatoOnClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.listatelefone.adapter.listener
-
-import com.example.listatelefone.model.Contato
-
-class ContatoOnClickListener (val clickListener: (contato: Contato)-> Unit){
-    fun onClick(contato: Contato) = clickListener
-}
--- app/src/main/java/com/example/listatelefone/viewmodel/DetalhesContatoViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.listatelefone.viewmodel
-
-class DetalhesContatoViewModel {
-}
